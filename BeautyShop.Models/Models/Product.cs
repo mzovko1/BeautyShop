c25909b70aafb42f92c9b335cb3bcfe48ee403fd
matea315 <mateadmatea@gmail.com>
2024-03-18 20:00:24 +0100

    Add project files.

---
+++ BeautyShop.Models/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeautyShop.Models.Models;
+
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Brand { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string Picture { get; set; }
+    public string SkinType { get; set; }
+    public Category Category { get; set; }
+}
